4ee28e6d44997b20f7998eefc0db59f0505b0069
Gerardo <rojasgerardo786@gmail.com>
2024-07-02 18:21:04 -0400

    app modified for i18n

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import { useEffect } from 'react';
+import { appWithTranslation } from 'next-i18next';
 import Layout from '../components/Layout'
-import ThemeToggle from '../components/ThemeToggle'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-    // Effect to apply the dark mode class if necessary
     useEffect(() => {
-        // Check if dark mode is enabled in localStorage
         const isDarkMode = localStorage.getItem('theme') === 'dark';
         if (isDarkMode) {
             document.documentElement.classList.add('dark');
@@ -18,13 +16,10 @@ function MyApp({ Component, pageProps }) {
     return (
         <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
             <Layout>
-                <div className="absolute top-4 right-4">
-                    <ThemeToggle />
-                </div>
                 <Component {...pageProps} />
             </Layout>
         </div>
     )
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
