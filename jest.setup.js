effbc3de7962d0bf56068e172bcd1fe738083428
Gerardo <rojasgerardo786@gmail.com>
2024-07-02 20:33:15 -0400

    Update Jest configuration and tests for i18n support
    - Modify jest.config.js to include moduleNameMapper
    - Add jest.setup.js for mocking next-i18next
    - Update about.test.js to work with i18n and LanguageTransitionProvider

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,21 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+
+jest.mock('next-i18next', () => ({
+    useTranslation: () => {
+        return {
+            t: (str) => str,
+            i18n: {
+                changeLanguage: () => new Promise(() => { }),
+            },
+        };
+    },
+    serverSideTranslations: () => {
+        return Promise.resolve({
+            _nextI18Next: {
+                initialI18nStore: {},
+                initialLocale: 'en',
+                userConfig: null,
+            },
+        });
+    },
+}));
